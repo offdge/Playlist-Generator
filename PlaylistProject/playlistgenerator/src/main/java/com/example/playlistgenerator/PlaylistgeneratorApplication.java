c866f3449d10671a5dd37faf82b50f5d5c8953f0
Hris <hrista.raleva@gmail.com>
2019-04-11 13:23:47 +0300

    initial Intellij project set-up

---
+++ PlaylistProject/playlistgenerator/src/main/java/com/example/playlistgenerator/PlaylistgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.playlistgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistgeneratorApplication.class, args);
+    }
+
+}
