255d0d8e74dcca37be3ac405001bc59f48be13c4
Hris <hrista.raleva@gmail.com>
2019-04-30 18:57:02 +0300

    Login/SignUp layouts created + 20190427 db uploaded

--- playlistgenerator/src/main/java/com/example/playlistgenerator/controllers/PlaylistRestController.java
+++ playlistgenerator/src/main/java/com/example/playlistgenerator/controllers/PlaylistRestController.java
@@ -5,6 +5,7 @@ import com.example.playlistgenerator.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class PlaylistRestController {
         this.service = service;
     }
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PostMapping("/createPlaylist/{genre}")
     public ResponseEntity createBeer (String genre) {
         service.createPlaylistByGenre(genre);
