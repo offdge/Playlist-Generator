860c7545869918a87c205f584fc57fef90b36894
Georgi Dekov <offdge@abv.bg>
2019-05-08 23:48:14 +0300

    AWS Elastic Beanstalk - jar file created.

--- playlistgenerator/src/main/java/com/example/playlistgenerator/PlaylistgeneratorApplication.java
+++ playlistgenerator/src/main/java/com/example/playlistgenerator/PlaylistgeneratorApplication.java
@@ -1,7 +1,5 @@
 package com.example.playlistgenerator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,20 +8,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class PlaylistgeneratorApplication {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PlaylistgeneratorApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(PlaylistgeneratorApplication.class, args);
-        LOGGER.info("log this");
-        LOGGER.info("log this");
-        LOGGER.info("log this");
-        LOGGER.info("log this");
-        LOGGER.info("log this");
-        LOGGER.info("log that");
-        LOGGER.info("log that");
-        LOGGER.info("log that");
-        LOGGER.info("log that");
-        LOGGER.info("log that {}", "sUPER RAZOCHAROVAN");
     }
 
     @Bean
